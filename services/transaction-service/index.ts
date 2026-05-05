88d8666340fd21e04032daef85d3a63c29d25b89
Dannie Hybrid <danniehybrid@gmail.com>
2026-05-05 22:07:52 +0100

    feat: implement basic transaction service with in-memory store

---
+++ services/transaction-service/index.ts
@@ -0,0 +1,16 @@
+import { Transaction } from "../../packages/types/transaction";
+
+const db: Record<string, Transaction> = {};
+
+export function createTransaction(tx: Transaction) {
+  tx.status = "PENDING_APPROVAL";
+  tx.approvals = [];
+
+  db[tx.id] = tx;
+
+  return tx;
+}
+
+export function getTransaction(id: string) {
+  return db[id];
+}
